ff4943cd7afab528ddb41a245b4c572eb2ccd989
Mohammed Abudahesh <willcodahsh@gmail.com>
2021-02-04 10:26:32 +0300

    fix issues to use expermintal with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+//@ts-ignore
+ReactDOM.unstable_createRoot(root, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
